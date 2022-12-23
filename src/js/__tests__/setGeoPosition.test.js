a5ce8c1fe9c82b2995767c662d9847ad0e8ec874
arkadii <88792842+Arkadii-2021@users.noreply.github.com>
2022-12-23 19:41:49 +0700

    Update setGeoPosition.test.js

--- src/js/__tests__/setGeoPosition.test.js
+++ src/js/__tests__/setGeoPosition.test.js
@@ -1,53 +1,12 @@
-import { checkGeoPos } from '../setGeoPosition';
-const el = document.createElement('main');
-el.classList.add('timeline');
-el.setAttribute('data-id', 'geo-coord');
-  
-const container = document.createElement('div');
-container.classList.add('login');
-    
-const confirmForm = document.createElement('form');
-confirmForm.classList.add('login_form');
-    
-const nickNameTitle = document.createElement('h3');
-nickNameTitle.textContent = 'Что-то пошло не так';
-  
-const nickNameDescription = document.createElement('p');
-nickNameDescription.textContent = 'К сожалениею, определить ваше местоположение нам не удалось. Пожалуйста, дайте разрешение на использование геолокации или введите координаты вручную.';
-    
-const loginContainer = document.createElement('div');
-loginContainer.classList.add('login_container');
-    
-const loginInput = document.createElement('input');
-loginInput.classList.add('login_input');
-loginInput.setAttribute('placeholder', '55.0207488, 82.9652992');
-    
-const ok = document.createElement('button');
-ok.setAttribute('type', 'submit');
-ok.textContent = 'ОК';
-  
-const cancel = document.createElement('button');
-cancel.classList.add('cancel');
-cancel.style = 'margin-left: 20px;';
-cancel.textContent = 'Отмена';
-document.body.append(el);
-  
-el.append(container);
-container.append(confirmForm);
-confirmForm.append(nickNameTitle, nickNameDescription, loginContainer, ok, cancel);
-loginContainer.append(loginInput);
+import { matchGeoPos } from '../setGeoPosition';
 
-const form = el.querySelector('.login_form');
+const coordlist = [
+  ['корректный ввод кординат', '51.50851, 0.12572', true],
+  ['ввод координат с отсутствием пробелов', '51.50851,0.12572', false],
+  ['ввод координат в квадратных скобках', '[51.50851, 0.12572]', false],
+];
 
-test('корректный ввод кординат', () => {
-  expect(checkGeoPos(ok, form, container, '51.50851, 0.12572')).toBe(true);
+const handler = test.each(coordlist);
+handler('test coords, value: %i, status: %s', (_, coord, isValid) => {
+  expect(matchGeoPos(coord)).toEqual(isValid);
 });
-
-test('ввод координат с отсутствием пробелов', () => {
-  expect(checkGeoPos(ok, form, container, '51.50851,0.12572')).toBe(false);
-});
-
-test('ввод координат в квадратных скобках', () => {
-  expect(checkGeoPos(ok, form, container, '[51.50851, 0.12572]')).toBe(false);
-});
-
